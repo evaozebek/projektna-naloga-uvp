28908b7896d66f3ba29bad1152d64195531ee510
Eva Ozebek <eva.ozebek@gmail.com>
2017-08-16 18:15:29 +0200

    Naredila sem vse gumbe, ki pa še nimajo funkcije

---
+++ projektna-naloga-uvp.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+okno = tk.Tk()
+
+#################
+
+
+# VSI GUMBI - RAZPOREJENI V GRID
+gumb0 = tk.Button(okno, text = "0")
+gumb0.grid(row = 5, column = 0)
+
+gumb1 = tk.Button(okno, text = "1")
+gumb1.grid(row = 4, column = 0)
+
+gumb2 = tk.Button(okno, text = "2")
+gumb2.grid(row = 4, column = 1)
+
+gumb3 = tk.Button(okno, text = "3")
+gumb3.grid(row = 4, column = 2)
+
+gumb4 = tk.Button(okno, text = "4")
+gumb4.grid(row = 3, column = 0)
+
+gumb5 = tk.Button(okno, text = "5")
+gumb5.grid(row = 3, column = 1)
+
+gumb6 = tk.Button(okno, text = "6")
+gumb6.grid(row = 3, column = 2)
+
+gumb7 = tk.Button(okno, text = "7")
+gumb7.grid(row = 2, column = 0)
+
+gumb8 = tk.Button(okno, text = "8")
+gumb8.grid(row = 2, column = 1)
+
+gumb9 = tk.Button(okno, text = "9")
+gumb9.grid(row = 2, column = 2)
+
+gumbplus = tk.Button(okno, text = "+")
+gumbplus.grid(row = 5, column = 3)
+
+gumbminus = tk.Button(okno, text = "-")
+gumbminus.grid(row = 4, column = 3)
+
+gumbkrat = tk.Button(okno, text = "x")
+gumbkrat.grid(row = 3, column = 3)
+
+gumbdeljeno = tk.Button(okno, text = "+")
+gumbdeljeno.grid(row = 2, column = 3)
+
+gumboklepajl = tk.Button(okno, text = "(")
+gumboklepajl.grid(row = 5, column = 1)
+
+gumboklepajd = tk.Button(okno, text = ")")
+gumboklepajd.grid(row = 5, column = 2)
+                         
+                         
+
+
+
+
+
+
+
+
+
+####################
+okno.mainloop()
